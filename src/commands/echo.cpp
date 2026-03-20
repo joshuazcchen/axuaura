6bdcf007460557152cef1167e4b8c11bdd99b0c0
joshua chen <joshuaz.chen@mail.utoronto.ca>
2026-03-20 19:08:59 -0400

    removed echo

--- src/commands/echo.cpp
+++ src/commands/echo.cpp
@@ -2,24 +2,25 @@
 #include <regex>
 
 namespace commands {
-    dpp::slashcommand get_echo_definition(dpp::cluster& bot) {
-        dpp::slashcommand cmd("echo", "echo", bot.me.id);
-        cmd.set_default_permissions(dpp::p_administrator);
-        cmd.set_dm_permission(false);
-        cmd.add_option(dpp::command_option(dpp::co_string, "content", "msg", true));
-        return cmd;
-    }
+	dpp::slashcommand get_echo_definition(dpp::cluster& bot) {
+		dpp::slashcommand cmd("echo", "echo", bot.me.id);
+		cmd.set_default_permissions(dpp::p_administrator);
+		cmd.set_dm_permission(false);
+		cmd.add_option(dpp::command_option(dpp::co_string, "content", "msg", true));
+		return cmd;
+	}
 
-    void handle_echo(const dpp::slashcommand_t& event, dpp::cluster& bot) {
-        std::string msg = std::get<std::string>(event.get_parameter("content"));
-        std::string user_id = std::to_string(event.command.get_issuing_user().id);
-
-        if (msg[0] != '!') {
-            event.reply(dpp::message(event.command.channel_id, msg + "\n-# Sent by <@" + user_id + ">."));
-        } else {
-            bot.message_create(dpp::message(event.command.channel_id, std::regex_replace(msg.substr(1), std::regex(R"(\\n)"), "\n")));
-            event.reply(dpp::message("ok").set_flags(dpp::m_ephemeral));
-        }
-    }
+	void handle_echo(const dpp::slashcommand_t& event, dpp::cluster& bot) {
+		//        std::string msg = std::get<std::string>(event.get_parameter("content"));
+		//        std::string user_id = std::to_string(event.command.get_issuing_user().id);
+		//
+		//        if (msg[0] != '!') {
+		//            event.reply(dpp::message(event.command.channel_id, msg + "\n-# Sent by <@" + user_id + ">."));
+		//        } else {
+		//            bot.message_create(dpp::message(event.command.channel_id, std::regex_replace(msg.substr(1), std::regex(R"(\\n)"), "\n")));
+		//            event.reply(dpp::message("ok").set_flags(dpp::m_ephemeral));
+		//        }
+		event.reply(dpp::message("command disabled").set_flags(dpp::m_ephemeral));
+	}
 
 }
