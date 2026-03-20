8b76ece54280894b7219ab99dd98885044447f19
joshua chen <joshuaz.chen@mail.utoronto.ca>
2026-03-20 18:35:01 -0400

    eeeeeee

--- src/commands/mkspl.cpp
+++ src/commands/mkspl.cpp
@@ -1,14 +1,18 @@
 #include "commands.h"
+#include <config.h>
 
-void handle_mkspl(const dpp::slashcommand_t& event, dpp::cluster& bot) {
-    dpp::snowflake usr = std::get<dpp::snowflake>(event.get_parameter("user"));
-    auto it = std::find(SPECIALS.begin(), SPECIALS.end(), usr);
-    
-    if (it != SPECIALS.end()) {
-        SPECIALS.erase(it);
-        event.reply(dpp::message("user removed").set_flags(dpp::m_ephemeral));
-    } else {
-        SPECIALS.push_back(usr);
-        event.reply(dpp::message("user added").set_flags(dpp::m_ephemeral));
-    }
+namespace commands {
+
+	void handle_mkspl(const dpp::slashcommand_t& event, dpp::cluster& bot) {
+		dpp::snowflake usr = std::get<dpp::snowflake>(event.get_parameter("user"));
+		auto it = std::find(SPECIALS.begin(), SPECIALS.end(), usr);
+
+		if (it != SPECIALS.end()) {
+			SPECIALS.erase(it);
+			event.reply(dpp::message("user removed").set_flags(dpp::m_ephemeral));
+		} else {
+			SPECIALS.push_back(usr);
+			event.reply(dpp::message("user added").set_flags(dpp::m_ephemeral));
+		}
+	}
 }
