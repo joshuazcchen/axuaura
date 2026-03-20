f68997c227aedd721eed3bebe2f990ab66aca8e7
joshua chen <joshuaz.chen@mail.utoronto.ca>
2026-03-20 18:37:33 -0400

    fix namespace

--- src/commands/mkspl.cpp
+++ src/commands/mkspl.cpp
@@ -2,16 +2,22 @@
 #include <config.h>
 
 namespace commands {
+	dpp::slashcommand get_mkspl_definition(dpp::cluster& bot) {
+		return dpp::slashcommand("mkspl", "make someone special", bot.me.id)
+			.set_default_permissions(dpp::p_administrator)
+			.set_dm_permission(false)
+			.add_option(dpp::command_option(dpp::co_user, "user", "who to toggle?", true));
+	}
 
 	void handle_mkspl(const dpp::slashcommand_t& event, dpp::cluster& bot) {
 		dpp::snowflake usr = std::get<dpp::snowflake>(event.get_parameter("user"));
-		auto it = std::find(SPECIALS.begin(), SPECIALS.end(), usr);
+		auto it = std::find(config::SPECIALS.begin(), config::SPECIALS.end(), usr);
 
-		if (it != SPECIALS.end()) {
-			SPECIALS.erase(it);
+		if (it != config::SPECIALS.end()) {
+			config::SPECIALS.erase(it);
 			event.reply(dpp::message("user removed").set_flags(dpp::m_ephemeral));
 		} else {
-			SPECIALS.push_back(usr);
+			config::SPECIALS.push_back(usr);
 			event.reply(dpp::message("user added").set_flags(dpp::m_ephemeral));
 		}
 	}
