06e28b1c07a9d50fb9ae1dbf4604e0e63e347ec3
Joshua Chen <joshuaz.chen@mail.utoronto.ca>
2026-05-03 02:10:35 +0000

    added credits into the other things that needed it

--- src/commands/credits.cpp
+++ src/commands/credits.cpp
@@ -8,7 +8,7 @@ namespace commands {
 		return cmd;
 	}
 
-	void handle_echo(const dpp::slashcommand_t& event, dpp::cluster& bot) {
+	void handle_credits(const dpp::slashcommand_t& event, dpp::cluster& bot) {
 		event.reply(dpp::message("bot by @corgisays\n-# ps: [give me money.](https://ko-fi.com/corig)"));
 	}
 
