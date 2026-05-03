541f407b043fabdeedc00ead9a4b7d3289a3677b
joshua <joshuaz.chen@mail.utoronto.ca>
2026-05-03 18:09:23 -0400

    added update to credits

--- src/commands/credits.cpp
+++ src/commands/credits.cpp
@@ -8,7 +8,7 @@ namespace commands {
 	}
 
 	void handle_credits(const dpp::slashcommand_t& event, dpp::cluster& bot) {
-		event.reply(dpp::message("bot by @corgisays\n-# ps: [give me money.](https://ko-fi.com/corig)"));
+		event.reply(dpp::message("bot by [@corgisays](<https://scallion.uk>)\n-# ps: [give me money.](<https://ko-fi.com/corig>)\n\nart by [@notaxuaxi](<https://www.instagram.com/notaxuaxi/>)\n-# subscribe to his [patreon](<https://www.patreon.com/cw/notaxuaxi>).\n\nfor all bot related inquiries, email `corgi@scallion.uk`."));
 	}
 
 }
