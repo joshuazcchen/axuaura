9b57fc239f13da22babad8c0a99824162f933117
joshua <joshuaz.chen@mail.utoronto.ca>
2026-06-07 18:40:48 -0700

    removed TODOs and aliases, decided not to use aliases.

--- include/db.h
+++ include/db.h
@@ -24,23 +24,6 @@ namespace db {
 	std::vector<std::pair<std::string, int>> get_ab(dpp::snowflake guild_id, int limit, bool bottom);
 	int get_total_aura(dpp::snowflake guild_id);
 	int aura_rank(dpp::snowflake guild_id, dpp::snowflake user_id);
-	// TODO: see db_aura.cpp
-	// TEMPORARY ALIASES:
-
-	inline int aura_get(dpp::snowflake guild_id, dpp::snowflake user_id) { return get_aura(guild_id, user_id); }
-	inline void aura_set(dpp::snowflake guild_id, dpp::snowflake user_id, int amount) {
-		return set_aura(guild_id, user_id, amount);
-	}
-	inline void aura_add(dpp::snowflake guild_id, dpp::snowflake user_id, int amount) {
-		return add_aura(guild_id, user_id, amount);
-	}
-	inline void aura_rmv(dpp::snowflake guild_id, dpp::snowflake user_id, int amount) {
-		return rmv_aura(guild_id, user_id, amount);
-	}
-	inline std::vector<std::pair<std::string, int>> ab_get(dpp::snowflake guild_id, int limit, bool bottom) {
-		return get_ab(guild_id, limit, bottom);
-	}
-	inline int aura_total(dpp::snowflake guild_id) { return get_total_aura(guild_id); }
 
 	// settings related
 	void set_setting(dpp::snowflake guild_id, const std::string& key, const std::string& val);
@@ -51,32 +34,6 @@ namespace db {
 	bool get_setting_bool(dpp::snowflake guild_id, const std::string& key, bool default_val = false);
 	std::vector<dpp::snowflake> settings_get_guilds_with(const std::string& key);
 
-	// TODO: REMOVE, TEMPORARY ALIAS
-	inline void setting_set(dpp::snowflake guild_id, const std::string& key, const std::string& val) {
-		set_setting(guild_id, key, val);
-	}
-
-	inline void setting_set(dpp::snowflake guild_id, const std::string& key, int val) {
-		set_setting(guild_id, key, val);
-	}
-
-	inline void setting_set(dpp::snowflake guild_id, const std::string& key, bool val) {
-		set_setting(guild_id, key, val);
-	}
-
-	inline std::string setting_get_str(dpp::snowflake guild_id, const std::string& key,
-									   std::string default_val = "corgi") {
-		return get_setting_str(guild_id, key, default_val);
-	}
-
-	inline int setting_get_int(dpp::snowflake guild_id, const std::string& key, int default_val = 7) {
-		return get_setting_int(guild_id, key, default_val);
-	}
-
-	inline bool setting_get_bool(dpp::snowflake guild_id, const std::string& key, bool default_val = false) {
-		return get_setting_bool(guild_id, key, default_val);
-	}
-
 	// polls related
 	struct Poll {
 		int p_id;
@@ -84,7 +41,6 @@ namespace db {
 		std::string ops;
 	};
 
-	// TODO: fix the stupid naming here or just remove polls entirely
 	std::vector<Poll> p_get_polls(dpp::snowflake guild_id);
 	Poll p_get_poll(int p_id);
 	int p_set_poll(dpp::snowflake guild_id, std::string title, std::string ops);
